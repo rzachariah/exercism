281441c395787de4948e9e776c2e40cadb080dc6
Ranjith Zachariah <rzachariah@ezesoft.com>
2019-10-13 17:21:00 -0400

    Simplify Pangram

--- csharp/pangram/Pangram.cs
+++ csharp/pangram/Pangram.cs
@@ -6,17 +6,6 @@ public static class Pangram
     const string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     public static bool IsPangram(string input)
     {
-        var alphabetMap = alphabet.ToCharArray().ToDictionary(c => c, _ => 0);
-        var inputAlphabetic = input
-            .ToUpper()
-            .Where(s => alphabet.Contains(s));
-        foreach (char c in inputAlphabetic)
-        {
-            alphabetMap[c]++;
-        }
-        int charsNotPresentCount = alphabetMap.Values
-            .Where(v => v == 0)
-            .Count();
-        return charsNotPresentCount == 0;
+        return 26 == input.Where(c => char.IsLetter(c)).Select(c => char.ToUpper(c)).Distinct().Count();
     }
 }
