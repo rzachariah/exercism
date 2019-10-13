e7ccc9f1f1f97627648001bb42ada35f078cf0ed
Ranjith Zachariah <rzachariah@ezesoft.com>
2019-10-13 12:56:28 -0400

    Initial

---
+++ csharp/acronym/Acronym.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+public static class Acronym
+{
+    public static string Abbreviate(string phrase)
+    {
+        var words = phrase
+            .Split(" ")
+            .SelectMany(w => w.Split("-"));
+        var firstLetters = words
+            .Where(w => w.Length > 0)
+            .Select(w => w.First(c => c != '_'));
+        return string.Join("", firstLetters).ToUpper();
+    }
+}
---
+++ csharp/grains/Grains.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+public static class Grains
+{
+    public static ulong Square(int n)
+    {
+        if (n < 1 || n > 64) throw new ArgumentOutOfRangeException(nameof(n));
+        return (ulong)Math.Pow(2, n - 1);
+    }
+
+    public static ulong Total()
+    {
+        return Enumerable.Range(1, 64)
+            .Select(Square)
+            .Aggregate((cur, x) => cur + x);
+    }
+}
